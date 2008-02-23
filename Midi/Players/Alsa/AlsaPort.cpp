36523b439c351dba14602a11ea6887fb9995624d
auria <48419909+mg186@users.noreply.github.com>
2008-02-23 20:19:57 +0000

    ?

---
+++ Midi/Players/Alsa/AlsaPort.cpp
@@ -0,0 +1,219 @@
+#include "Midi/Players/Alsa/AlsaPort.h"
+#include <iostream>
+#include "wx/wx.h"
+#include "IO/IOUtils.h"
+
+namespace AriaMaestosa
+{
+
+
+
+MidiDevice::MidiDevice(MidiContext* context, int client_arg, int port_arg, const char* name_arg)
+{
+    client = client_arg;
+    port = port_arg;
+    name = fromCString(name_arg);
+    MidiDevice::midiContext = context;
+}
+
+MidiDevice* MidiContext::getDevice(int i)
+{
+    if(i<0 or i>devices.size()) return NULL;
+    return &devices[i];
+}
+
+MidiDevice* MidiContext::getDevice(int client, int port)
+{
+    for(int n=0; n<devices.size(); n++)
+    {
+        if(devices[n].client == client and devices[n].port == port) return &devices[n];
+    }
+    return NULL;
+}
+
+bool MidiDevice::open()
+{
+	address.client = client;
+	address.port = port;
+
+	snd_seq_port_subscribe_alloca(&midiContext->subs);
+
+	snd_seq_port_subscribe_set_sender(midiContext->subs, &midiContext->address);
+	snd_seq_port_subscribe_set_dest(midiContext->subs, &address);
+
+	const int success = snd_seq_subscribe_port(midiContext->sequencer, midiContext->subs);
+	if(success != 0)
+    {
+        wxMessageBox( _("Failed to open ALSA port.") );
+        return false;
+    }
+    return true;
+}
+
+
+void MidiDevice::close()
+{
+	//snd_seq_port_subscribe_alloca(&midiContext->subs);
+	//snd_seq_port_subscribe_set_sender(midiContext->subs, &midiContext->address);
+	//snd_seq_port_subscribe_set_dest(midiContext->subs, &address);
+
+	snd_seq_unsubscribe_port(midiContext->sequencer, midiContext->subs);
+	snd_seq_drop_output(midiContext->sequencer);
+    snd_seq_free_queue(midiContext->sequencer, midiContext->queue);
+    snd_seq_close(midiContext->sequencer);
+
+	//snd_seq_port_subscribe_free(midiContext.subs);
+}
+
+
+#pragma mark -
+
+void MidiContext::closeDevice()
+{
+    device->close();
+}
+
+bool MidiContext::askOpenDevice()
+{
+    findDevices();
+
+    // -------------- ask user to choose a midi port -----------------
+    const int deviceAmount = getDeviceAmount();
+
+    if(deviceAmount < 1)
+    {
+        wxMessageBox( _("No midi port available, aborting.") );
+        exit(1);
+    }
+
+    wxString choices[deviceAmount];
+    MidiDevice* currentDevice;
+    for(int n=0; n<deviceAmount; n++)
+    {
+        currentDevice = getDevice(n);
+        choices[n] =    to_wxString(currentDevice->client) +
+                        wxString( wxT(":") ) +
+                        to_wxString(currentDevice->port) +
+                        wxString( wxT(" (") ) +
+                        currentDevice->name +
+                        wxString( wxT(")") );
+    }
+
+    int userChoice = wxGetSingleChoiceIndex(_("Select an ALSA midi port to use for output."),
+                                            _("ALSA midi port choice"),
+                                            deviceAmount, choices);
+    if(userChoice == -1) return false;
+
+    bool success = openDevice(getDevice(userChoice));
+    if(success) return true;
+    else
+    {
+        wxMessageBox( _("The selected ALSA device could not be opened.") );
+        return askOpenDevice();
+    }
+    return false;
+}
+
+void MidiContext::findDevices()
+{
+    devices.clear();
+	snd_seq_client_info_t* clientInfo;
+
+	if (snd_seq_open(&sequencer, "default", SND_SEQ_OPEN_OUTPUT, 0) < 0)
+	{
+		return;
+	}
+
+	snd_seq_client_info_alloca(&clientInfo);
+	snd_seq_client_info_set_client(clientInfo, -1);
+
+	// iterate through clients
+	while (snd_seq_query_next_client(sequencer, clientInfo) >= 0)
+	{
+		snd_seq_port_info_t* pinfo;
+
+		snd_seq_port_info_alloca(&pinfo);
+		snd_seq_port_info_set_client(pinfo, snd_seq_client_info_get_client(clientInfo));
+		snd_seq_port_info_set_port(pinfo, -1);
+
+		// and now through ports
+		while (snd_seq_query_next_port(sequencer, pinfo) >= 0)
+		{
+			unsigned int capability = snd_seq_port_info_get_capability(pinfo);
+
+			if ((capability & SND_SEQ_PORT_CAP_SUBS_WRITE) == 0)
+			{
+				continue;
+			}
+
+			int client  = (snd_seq_port_info_get_addr(pinfo))->client;
+			int port    = (snd_seq_port_info_get_addr(pinfo))->port;
+
+			std::cout << client << ":" << port << " --> " << snd_seq_port_info_get_name(pinfo) << std::endl;
+			devices.push_back( MidiDevice(this, client, port, snd_seq_port_info_get_name(pinfo)) );
+
+		}
+
+	}
+
+	if (snd_seq_set_client_name(sequencer, "Aria") < 0)
+	{
+		return;
+	}
+
+	//midiContext.address.port = snd_seq_create_simple_port(midiContext.sequencer, "Aria Port 0", SND_SEQ_PORT_CAP_SUBS_WRITE, SND_SEQ_PORT_TYPE_APPLICATION);
+    address.port =  snd_seq_create_simple_port(sequencer, "Aria Port 0",
+					 SND_SEQ_PORT_CAP_WRITE |
+					 SND_SEQ_PORT_CAP_SUBS_WRITE |
+					 SND_SEQ_PORT_CAP_READ,
+					 SND_SEQ_PORT_TYPE_APPLICATION);
+					 //SND_SEQ_PORT_TYPE_MIDI_GENERIC);
+
+	address.client = snd_seq_client_id (sequencer);
+	queue = snd_seq_alloc_queue (sequencer);
+
+	//snd_seq_set_client_pool_output (midiContext.sequencer, 1024);
+	snd_seq_set_client_pool_output (sequencer, 1024);
+
+	destlist = g_array_new(0, 0, sizeof(snd_seq_addr_t));
+	// snd_seq_set_client_pool_output(seq_handle, (seq_len<<1) + 4);
+}
+
+int MidiContext::getDeviceAmount()
+{
+    return devices.size();
+}
+
+MidiContext::MidiContext()
+{
+}
+
+bool MidiContext::isPlaying()
+{
+    return timerStarted;
+}
+void MidiContext::setPlaying(bool playing)
+{
+    timerStarted = playing;
+}
+
+bool MidiContext::openDevice(MidiDevice* device)
+{
+    MidiContext::device = device;
+    if(device == NULL)
+    {
+        /*
+        wxMessageBox(   wxString(_("Failed to open ALSA port ")) +
+                        to_wxString(device->client) +
+                        wxString( wxT(":") ) +
+                        to_wxString(device->port) );
+        askOpenDevice();
+        */
+        return false;
+        //exit(1);
+    }
+    return device->open();
+}
+
+
+}
---
+++ Midi/Players/Alsa/seqmidi.cpp
@@ -0,0 +1,309 @@
+/*
+ * File: seqmidi.m - convert to the sequencer events
+ *
+ * Copyright (C) 1999 Steve Ratcliffe
+ * with major modifications from Aria Meastosa author - 2007.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+#ifdef _PMIDI_ALSA
+
+#include <iostream>
+
+#include "glib.h"
+//#include "Midi/Players/pmidi/glib.h"
+#include "Midi/Players/pmidi/elements.h"
+
+#include <string.h>
+#include "Midi/Players/pmidi/seqlib.h"
+#include "Midi/Players/pmidi/seqpriv.h"
+
+namespace AriaMaestosa
+{
+//#include "Midi/Players/pmidi/glib.h"
+
+//extern "C"
+//{
+/*
+ * Initialize a midi event from the context. The source and
+ * destination addresses will be set from the information
+ * stored in the context, which depends on the previous
+ * connection calls. In addition the time and channel are set.
+ * This should be called first before any of the following
+ * functions.
+ *
+ *  Arguments:
+ *    ctxp      - Client application
+ *    ep        - Event to init
+ *    time      - Midi time
+ *    devchan   - Midi channel
+ */
+void
+seq_midi_event_init(seq_context_t *ctxp, snd_seq_event_t *ep,
+        unsigned long time, int devchan)
+{
+	int  dev;
+
+	dev = devchan >> 4;
+
+	/*
+	 * If insufficient output devices have been registered, then we
+	 * just scale the device back to fit in the correct range.  This
+	 * is not necessarily what you want.
+	 */
+	if (dev >= ctxp->ctxndest)
+		dev = dev % ctxp->ctxndest;
+
+	snd_seq_ev_clear(ep);
+	snd_seq_ev_schedule_tick(ep, ctxp->queue, 0, time);
+	ep->source = ctxp->source;
+	if (ctxp->ctxndest > 0)
+		ep->dest = g_array_index(ctxp->destlist, snd_seq_addr_t, dev);
+}
+
+/*
+ * Send a note event.
+ *  Arguments:
+ *    ctxp      - Client context
+ *    ep        - Event template
+ *    note      - Pitch of noten declared
+/usr/lib/gcc/powerpc-linux-gnu/4.1.2/../../../../include/c++/4.1.2/cstdio:116: error: '::freopen' has not been declared
+/usr/lib/gcc/powerpc-linux-gnu/4.1.2/../../../../include/c++/4.1.2/cstdio:117: error: '::fscanf' has not been d
+ *    vel       - Velocity of note
+ *    length    - Length of note
+ */
+void
+seq_midi_note(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int note, int vel,
+        int length)
+{
+	ep->type = SND_SEQ_EVENT_NOTE;
+
+	ep->data.note.channel = devchan & 0xf;
+	ep->data.note.note = note;
+	ep->data.note.velocity = vel;
+	ep->data.note.duration = length;
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a note on event.
+ *  Arguments:
+ *    ctxp      - Client context
+ *    ep        - Event template
+ *    note      - Pitch of note
+ *    vel       - Velocity of note
+ *    length    - Length of note
+ */
+void
+seq_midi_note_on(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int note, int vel,
+        int length)
+{
+	ep->type = SND_SEQ_EVENT_NOTEON;
+
+	ep->data.note.channel = devchan & 0xf;
+	ep->data.note.note = note;
+	ep->data.note.velocity = vel;
+	ep->data.note.duration = length;
+
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a note off event.
+ *  Arguments:
+ *    ctxp      - Client context
+ *    ep        - Event template
+ *    note      - Pitch of note
+ *    vel       - Velocity of note
+ *    length    - Length of note
+ */
+void
+seq_midi_note_off(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int note, int vel,
+        int length)
+{
+	ep->type = SND_SEQ_EVENT_NOTEOFF;
+
+	ep->data.note.channel = devchan & 0xf;
+	ep->data.note.note = note;
+	ep->data.note.velocity = vel;
+	ep->data.note.duration = length;
+
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a key pressure event.
+ *  Arguments:
+ *    ctxp      - Application context
+ *    ep        - Event template
+ *    note      - Note to be altered
+ *    value     - Pressure value
+ */
+void
+seq_midi_keypress(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int note,
+        int value)
+{
+	ep->type = SND_SEQ_EVENT_KEYPRESS;
+
+	ep->data.control.channel = devchan & 0xf;
+	ep->data.control.param = note;
+	ep->data.control.value = value;
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a control event.
+ *  Arguments:
+ *    ctxp      - Application context
+ *    ep        - Event template
+ *    control   - Controller to change
+ *    value     - Value to set it to
+ */
+void
+seq_midi_control(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int control,
+        int value)
+{
+	ep->type = SND_SEQ_EVENT_CONTROLLER;
+
+	ep->data.control.channel = devchan & 0xf;
+	ep->data.control.param = control;
+	ep->data.control.value = value;
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a program change event.
+ *  Arguments:
+ *    ctxp      - Application context
+ *    ep        - Event template
+ *    program   - Program to set
+ */
+void
+seq_midi_program(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int program)
+{
+	ep->type = SND_SEQ_EVENT_PGMCHANGE;
+
+	ep->data.control.channel = devchan & 0xf;
+	ep->data.control.value = program;
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a channel pressure event.
+ *  Arguments:
+ *    ctxp      - Application context
+ *    ep        - Event template
+ *    pressure  - Pressure value
+ */
+void
+seq_midi_chanpress(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int pressure)
+{
+	ep->type = SND_SEQ_EVENT_CHANPRESS;
+
+	ep->data.control.channel = devchan & 0xf;
+	ep->data.control.value = pressure;
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a pitchbend message. The bend parameter is centered on
+ * zero, negative values mean a lower pitch.
+ *  Arguments:
+ *    ctxp      - Application context
+ *    ep        - Event template
+ *    bend      - Bend value, centered on zero.n declared
+/usr/lib/gcc/powerpc-linux-gnu/4.1.2/../../../../include/c++/4.1.2/cstdio:116: error: '::freopen' has not been declared
+/usr/lib/gcc/powerpc-linux-gnu/4.1.2/../../../../include/c++/4.1.2/cstdio:117: error: '::fscanf' has not been d
+ */
+void
+seq_midi_pitchbend(seq_context_t *ctxp, snd_seq_event_t *ep, int devchan, int bend)
+{
+	ep->type = SND_SEQ_EVENT_PITCHBEND;
+
+	ep->data.control.channel = devchan & 0xf;
+	ep->data.control.value = bend;
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a tempo event. The tempo parameter is in microseconds
+ * per beat.
+ *  Arguments:n declared
+/usr/lib/gcc/powerpc-linux-gnu/4.1.2/../../../../include/c++/4.1.2/cstdio:116: error: '::freopen' has not been declared
+/usr/lib/gcc/powerpc-linux-gnu/4.1.2/../../../../include/c++/4.1.2/cstdio:117: error: '::fscanf' has not been d
+ *    ctxp      - Application context
+ *    ep        - Event template
+ *    tempo     - New tempo in usec per beat
+ */
+void
+seq_midi_tempo(seq_context_t *ctxp, snd_seq_event_t *ep, int tempo)
+{
+	ep->type = SND_SEQ_EVENT_TEMPO;
+
+	ep->data.queue.queue = ctxp->queue;
+	ep->data.queue.param.value = tempo;
+	ep->dest.client = SND_SEQ_CLIENT_SYSTEM;
+	ep->dest.port = SND_SEQ_PORT_SYSTEM_TIMER;
+	seq_write(ctxp, ep);
+}
+
+/*
+ * Send a sysex event. The status byte is to distiguish
+ * continuation sysex messages.
+ *  Arguments:
+ *    ctxp      - Application context
+ *    ep        - Event template
+ *    status    - Status byte for sysex
+ *    data      - Data to send
+ *    length    - Length of data
+ */
+void
+seq_midi_sysex(seq_context_t *ctxp, snd_seq_event_t *ep, int status,
+        unsigned char *data, int length)
+{
+	unsigned char* ndata;
+	int  nlen;
+
+	ep->type = SND_SEQ_EVENT_SYSEX;
+
+	ndata = (unsigned char*)g_malloc(length + 1);
+	nlen = length +1;
+
+	ndata[0] = status;
+	memcpy(ndata+1, data, length);
+
+	snd_seq_ev_set_variable(ep, nlen, ndata);
+
+	seq_write(ctxp, ep);
+
+	g_free(ndata);
+}
+
+/*
+ * Send an echo event back to the source client at the specified
+ * time.
+ *
+ *  Arguments:
+ *    ctxp      - Application context
+ *    time      - Time of event
+ */
+void
+seq_midi_echo(seq_context_t *ctxp, unsigned long time)
+{
+	snd_seq_event_t ev;
+
+	seq_midi_event_init(ctxp, &ev, time, 0);
+	/* Loop back */
+	ev.dest = ctxp->source;
+	seq_write(ctxp, &ev);
+}
+//}
+}
+#endif
