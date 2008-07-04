0eec643171284e009ebf1cb820c96f204add6ef8
auria <48419909+mg186@users.noreply.github.com>
2008-07-04 01:07:29 +0000

    Cleanup and restructuration towards being able to print something else thans tabs (e.g. scores...)

--- Printing/PrintingBase.h
+++ Printing/PrintingBase.h
@@ -22,16 +22,62 @@
 #include "Printing/PrintLayout.h"
 #include "wx/wx.h"
 
+/*
+ Printing module overview :
+ 
+ An 'AriaPrintable' object is created. Then tracks are added to it through 'addTrack'.
+ When each new track is added, its editor type is checked (guitar, score, etc.)
+ A child class of 'EditorPrintable' that can print this type of notation is then
+ created and added into a vector.
+ 
+ Then 'calculateLayout()' is called. This takes us into 'PrintLayout.h'. This part lays
+ out the measures and other elements in pages, and finds repetitions if necessary. It
+ builds a tree of lines (that can contain one ore more tracks), layout elements
+ (can be a measure, a repetition, etc.)
+ 
+ Once the song structure is built,
+ the AriaPrintable is sent to the printer. The method 'printPage' is then called
+ for each page. The AriaPrintable will then render the page header, etc. and split the
+ remaining space between tracks. Then, for each line, the method 'printYourself'
+ is called. There, the line further divides itself into individual tracks and calls
+ the corresponding EditorPrintable to draw itself.
+ 
+ The EditorPrintable base class manages stuff like drawing measures, repetitions,
+ determining the position of notes, etc. Only the part of the drawing that is specific
+ to a notation mode is left to the child. 'drawLine' is to b overridden by children.
+ Children are to call 'beginLine' early. Then they can iterate through LayoutElements
+ by calling getNextElement() in a loop until NULL is returned. Then, for
+ returned elements of type SINGLE_MEASURE, the child can get note information by
+ using getFIrstNoteInElement(), getLastNoteInElement(), getNotePrintX() and render
+ them appropriately.
+ 
+ */
 namespace AriaMaestosa
 {
     class Track;
     
 class EditorPrintable
 {
+    // keeps some info on the track currently being printed
+    int x0, y0, x1, y1;
+    bool show_measure_number;
+    int xloc, currentLayoutElement;
+    LayoutLine* currentLine;
+    int layoutElementsAmount;
+    wxDC* dc;
+protected:
+    float widthOfAChar;
 public:
-        EditorPrintable();
-        virtual ~EditorPrintable();
-        virtual void drawLine(LayoutLine& line, wxDC& dc, const int x0, const int y0, const int x1, const int y1, bool show_measure_number);
+    EditorPrintable();
+    virtual ~EditorPrintable();
+        
+    virtual void drawLine(LayoutLine& line, wxDC& dc, const int x0, const int y0, const int x1, const int y1, bool show_measure_number);
+
+    void beginLine(wxDC* dc, LayoutLine* line, int x0, const int y0, const int x1, const int y1, bool show_measure_number);
+    int getCurrentElementXStart();
+    int getCurrentElementXEnd();
+    LayoutElement* getNextElement();
+    int getNotePrintX(int noteID);
 };
     
 class AriaPrintable
--- Printing/TabPrint.cpp
+++ Printing/TabPrint.cpp
@@ -28,12 +28,10 @@ TablaturePrintable::~TablaturePrintable()
 
 void TablaturePrintable::drawLine(LayoutLine& line, wxDC& dc, const int x0, const int y0, const int x1, const int y1, bool show_measure_number)
 {
-    static const int line_width = 1;
-    
     Track* track = line.getTrack();
     
     // draw tab background
-    dc.SetPen(  wxPen( wxColour(125,125,125), line_width ) );
+    dc.SetPen(  wxPen( wxColour(125,125,125), 1 ) );
     
     const float stringHeight = (float)(y1 - y0) / (float)(string_amount-1);
     
@@ -43,113 +41,41 @@ void TablaturePrintable::drawLine(LayoutLine& line, wxDC& dc, const int x0, cons
         dc.DrawLine(x0, y, x1, y);
     }
     
-    std::vector<LayoutElement>& layoutElements = line.layoutElements;
+    //std::vector<LayoutElement>& layoutElements = line.layoutElements;
     
-    const int layoutElementsAmount = layoutElements.size();
+    //const int layoutElementsAmount = layoutElements.size();
     
-    int xloc = 2;
     
-    // 2 spaces allocated for left area of the tab
-    float widthOfAChar = (float)(x1 - x0) / (float)(line.charWidth+2);
+    beginLine(&dc, &line, x0, y0, x1, y1, show_measure_number);
     
-    assertExpr(line.charWidth,>,0);
-    assertExpr(widthOfAChar,>,0);
-    
-    for(int n=0; n<layoutElementsAmount; n++)
+    LayoutElement* currentElement;
+    while((currentElement = getNextElement()) and (currentElement != NULL))
     {
-        const int meas_x_start = x0 + (int)round(xloc*widthOfAChar) - widthOfAChar;
-        const int meas_x_end = x0 + (int)round((xloc+layoutElements[n].charWidth)*widthOfAChar);
-        const int mesa_w = meas_x_end - meas_x_start;
+        if(currentElement->type != SINGLE_MEASURE) continue;
         
-        // draw vertical line that starts measure
-        dc.SetPen(  wxPen( wxColour(0,0,0), line_width*2 ) );
-        dc.DrawLine( meas_x_start, y0, meas_x_start, y1);
+        const int firstNote = line.getFirstNoteInElement(currentElement);
+        const int lastNote = line.getLastNoteInElement(currentElement);
         
-        dc.SetTextForeground( wxColour(0,0,255) );
-
-        // ****** empty measure
-        if(layoutElements[n].type == EMPTY_MEASURE)
+        for(int i=firstNote; i<lastNote; i++)
         {
+            const int string = track->getNoteString(i);
+            const int fret = track->getNoteFret(i);
             
-        } 
-        // ****** repetitions
-        else if(layoutElements[n].type == SINGLE_REPEATED_MEASURE or layoutElements[n].type == REPEATED_RIFF)
-        {
-            // FIXME - why do I cut apart the measure and not the layout element?
-            /*
-            if(measures[layoutElements[n].measure].cutApart)
-			{
-                // TODO...
-                //dc.SetPen(  wxPen( wxColour(0,0,0), line_width*4 ) );
-                //dc.DrawLine( meas_x_start, y0, meas_x_start, y1);
-			}
-			*/
+            if(fret < 0)  dc.SetTextForeground( wxColour(255,0,0) );
             
-			wxString message;
-			if(layoutElements[n].type == SINGLE_REPEATED_MEASURE)
-			{
-				message = to_wxString(line.getMeasureForElement(n).firstSimilarMeasure+1);
-			}
-			else if(layoutElements[n].type == REPEATED_RIFF)
-			{
-				message =	to_wxString(layoutElements[n].firstMeasureToRepeat+1) +
-				wxT(" - ") + 
-				to_wxString(layoutElements[n].lastMeasureToRepeat+1);
-			}
+            // substract from width to leave some space on the right (coordinate is from the left of the text string so we need extra space on the right)
+            // if fret number is greater than 9, the string will have two characters so we need to recenter it a bit more
+            const int drawX = getNotePrintX(i) + (fret > 9 ? widthOfAChar/4 : widthOfAChar/2);
+            const int drawY = y0 + stringHeight*string - getCurrentPrintable()->text_height_half*0.8;
+            wxString label = to_wxString(fret);
             
-            dc.DrawText( message, meas_x_start + widthOfAChar/2, (y0+y1)/2 - getCurrentPrintable()->text_height_half );
+            dc.DrawText( label, drawX, drawY );
+            
+            if(fret < 0)  dc.SetTextForeground( wxColour(0,0,0) );
         }
-        // ****** play again
-        else if(layoutElements[n].type == PLAY_MANY_TIMES)
-        {
-            wxString label(wxT("X"));
-            label << layoutElements[n].amountOfTimes;
-            dc.DrawText( label, meas_x_start + widthOfAChar/2, (y0+y1)/2 - getCurrentPrintable()->text_height_half );
-        }
-        // ****** normal measure
-        else if(layoutElements[n].type == SINGLE_MEASURE)
-        {  
-            // draw measure ID
-            if(show_measure_number)
-            {
-                wxString measureLabel;
-                measureLabel << (line.getMeasureForElement(n).id+1);
-                dc.DrawText( measureLabel, meas_x_start - widthOfAChar/4, y0 - getCurrentPrintable()->text_height*1.4 );
-            }
-            
-            dc.SetTextForeground( wxColour(0,0,0) );
-            
-            
-            const int firstNote = line.getFirstNoteInElement(n);
-            const int lastNote = line.getLastNoteInElement(n);
-            const int firstTick = line.getMeasureForElement(n).firstTick;
-            const int lastTick = line.getMeasureForElement(n).lastTick;
-            
-            for(int i=firstNote; i<lastNote; i++)
-            {
-                const int tick = track->getNoteStartInMidiTicks(i);
-                const int string = track->getNoteString(i);
-                const int fret = track->getNoteFret(i);
-                
-                const float nratio = ((float)(tick - firstTick) / (float)(lastTick - firstTick));
-                
-                if(fret < 0)  dc.SetTextForeground( wxColour(255,0,0) );
-                
-                // substract from width to leave some space on the right (coordinate is from the left of the text string so we need extra space on the right)
-                // if fret number is greater than 9, the string will have two characters so we need to recenter it a bit more
-                const int drawX = nratio * (mesa_w-widthOfAChar*1.5) + meas_x_start + (fret > 9 ? widthOfAChar/4 : widthOfAChar/2);
-                const int drawY = y0 + stringHeight*string - getCurrentPrintable()->text_height_half*0.8;
-                wxString label = to_wxString(fret);
-                
-                dc.DrawText( label, drawX, drawY );
-                
-                if(fret < 0)  dc.SetTextForeground( wxColour(0,0,0) );
-            }
-        }
+        // }
         
-        xloc += layoutElements[n].charWidth;
-        
-    }//next element
+}//next element
 }
 
 
