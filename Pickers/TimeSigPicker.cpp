464b4b81eefbcdf3f07c7208915c8196d8251b81
auria <48419909+mg186@users.noreply.github.com>
2008-12-09 21:20:09 +0000

    renamed wrongly-named KeySigPicker to TimeSigPicker

--- Pickers/TimeSigPicker.cpp
+++ Pickers/TimeSigPicker.cpp
@@ -14,7 +14,7 @@
  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "Pickers/KeySigPicker.h"
+#include "Pickers/TimeSigPicker.h"
 #include "GUI/MainFrame.h"
 
 #include <iostream>
