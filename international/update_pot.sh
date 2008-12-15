19038a25ab8778c1dee3af45737f053807f5d79d
auria <48419909+mg186@users.noreply.github.com>
2008-12-15 02:02:03 +0000

    updated strings and french translation

--- international/update_pot.sh
+++ international/update_pot.sh
@@ -1,4 +1,3 @@
-cd ..
 CPP_FILE_LIST=`find . -name '*.cpp' -print`
 echo $CPP_FILE_LIST
-xgettext -d AriaS -s --keyword=_ --add-comments="I18N:" -p international -o aria_maestosa.pot $CPP_FILE_LIST
+xgettext -d AriaS -s --keyword=_ --add-comments="I18N:" -p ./international -o aria_maestosa.pot $CPP_FILE_LIST
