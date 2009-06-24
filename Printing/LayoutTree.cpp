911c6511e387632402ca86d57bc5b1feae093525
auria <48419909+mg186@users.noreply.github.com>
2009-06-24 00:43:34 +0000

    Split printing in more files, to organize this part of code better

---
+++ Printing/LayoutTree.cpp
@@ -0,0 +1,441 @@
+/*
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+#include "LayoutTree.h"
+#include "AriaCore.h"
+#include "Midi/MeasureData.h"
+#include "Midi/Track.h"
+#include "Printing/PrintingBase.h"
+
+namespace AriaMaestosa
+{
+const MeasureToExport nullMeasure(-1);
+
+MeasureToExport::MeasureToExport(const int measID)
+{
+    shortestDuration = -1;
+    firstSimilarMeasure = -1;
+    cutApart = false;
+    id = measID;
+    
+    if(measID != -1)
+    {
+        firstTick = getMeasureData()->firstTickInMeasure( measID );
+        lastTick = getMeasureData()->lastTickInMeasure( measID );
+    }
+}
+
+bool MeasureToExport::calculateIfMeasureIsSameAs(MeasureToExport& checkMeasure)
+{
+    
+    const int trackRefAmount = trackRef.size();
+    int total_note_amount = 0;
+    
+    for(int tref=0; tref<trackRefAmount; tref++)
+    {
+        const int my_first_note = trackRef[tref].firstNote;
+        const int my_last_note = trackRef[tref].lastNote;
+        const int his_first_note = checkMeasure.trackRef[tref].firstNote;
+        const int his_last_note = checkMeasure.trackRef[tref].lastNote;
+        
+        assert( trackRef.size() == checkMeasure.trackRef.size() );
+        assertExpr( tref,<,(int)trackRef.size() );
+        assert( trackRef[tref].track == checkMeasure.trackRef[tref].track );
+        Track* track = trackRef[tref].track;
+        
+        // if these 2 measures don't even have the same number of notes, they're definitely not the same
+        if( (his_last_note - his_first_note + 1) != (my_last_note - my_first_note + 1) )
+        {
+            return false;
+        }
+        
+        
+        const int noteAmount = (his_last_note - his_first_note);
+        total_note_amount += noteAmount;
+        
+        // don't count empty measures as repetitions 
+        if(noteAmount<1)
+        {
+            // when comparing a multiple-track line, don't stop on empty measures, for other tracks may not be empty
+            // for multi-track lines, variable 'total_note_amount' will be checked at the end to verify measure is not empty
+            if(trackRefAmount>1) continue;
+            return false;
+        }
+        
+        /*
+         if we get till there, the 2 measures have the same amount of notes.
+         to know whether they are truly identitcal, we need to compare note by note
+         we will match each notes from the first measure to the identical one in the second.
+         If ever one note failes to be matched, then the 2 measures are different.
+         */
+        int noteMatched_this[noteAmount];
+        int noteMatched_other[noteAmount];
+        for(int n=0; n<noteAmount; n++)
+        {
+            noteMatched_this[n] = false;
+            noteMatched_other[n] = false;
+        }
+        
+        for(int checkNote_this=0; checkNote_this<=noteAmount; checkNote_this++)
+        {
+            for(int checkNote_other=0; checkNote_other<=noteAmount; checkNote_other++)
+            {
+                if(noteMatched_other[checkNote_other]) continue; // this note was already matched
+                
+                // check start tick matches
+                if(track->getNoteStartInMidiTicks(his_first_note + checkNote_other) - checkMeasure.firstTick !=
+                   track->getNoteStartInMidiTicks(my_first_note + checkNote_this) - firstTick)
+                {
+                    // they dont match, check the next one
+                    continue;
+                }
+                
+                // check end tick matches
+                if(track->getNoteEndInMidiTicks(his_first_note + checkNote_other) - checkMeasure.firstTick !=
+                   track->getNoteEndInMidiTicks(my_first_note + checkNote_this) - firstTick)
+                {
+                    // they dont match, check the next one
+                    continue;
+                }
+                
+                // check pitch matches
+                if(track->getNotePitchID(his_first_note + checkNote_other) !=
+                   track->getNotePitchID(my_first_note + checkNote_this))
+                {
+                    // they dont match, check the next one
+                    continue;
+                }
+                
+                noteMatched_this[checkNote_this] = true;
+                noteMatched_other[checkNote_other] = true;
+                
+                
+            }//next note
+            
+            // we couldn't find a note in the other measure that matches this one
+            if(noteMatched_this[checkNote_this] == false)
+            {
+                // std::cout << "  not the same cause couldn't find a note in the other measure that matches this one\n";
+                return false;
+            }
+            
+        }//next note
+        
+    } // next track reference
+    
+    if(total_note_amount == 0) return false; // don't count empty measures as repeitions
+    return true;
+}
+
+int MeasureToExport::addTrackReference(const int firstNote, Track* track)
+{
+    const int noteAmount = track->getNoteAmount();
+    
+    MeasureTrackReference* newTrackRef = new MeasureTrackReference();
+    newTrackRef->track = track;
+    
+    // if firstNote is -1, it means all notes were processed. just add the track ref without searching for notes
+    if(firstNote == -1)
+    {
+        newTrackRef->firstNote = -1;
+        newTrackRef->lastNote = -1;
+        trackRef.push_back( newTrackRef );
+        return -1;
+    }
+    
+    // first note in measure
+    newTrackRef->firstNote = firstNote;
+    if(firstNote >= noteAmount) newTrackRef->firstNote = noteAmount-1;
+    
+    // find what the first, last and shortest note in current measure
+    int lastNote = firstNote;
+    int last_note_end = -1, last_note_start = -1;
+    
+    bool measure_empty = true;
+    for(int note=newTrackRef->firstNote; note<noteAmount; note++)
+    {
+        const int start_tick = track->getNoteStartInMidiTicks(note);
+        const int end_tick = track->getNoteEndInMidiTicks(note);
+        const int currentNoteDuration = end_tick - start_tick;
+        
+        if(currentNoteDuration <= 0)  continue; // skip malformed notes if any
+        
+        // stop when we're at next measure
+        if( start_tick >= lastTick ) break;
+        
+        // find last note - if many notes end at the same time, keep the one that started last
+        if(start_tick > last_note_start || end_tick > last_note_start ||
+           (end_tick == last_note_end && start_tick >= last_note_start)
+           )
+        {
+            lastNote = note;
+            last_note_end = end_tick;
+            last_note_start = start_tick;
+            measure_empty = false;
+        }
+        
+        // store duration if it's the shortest yet (but ignore dead/instant-hit notes)
+        const float relativeLength = (end_tick - start_tick) / (float)(getMeasureData()->beatLengthInTicks()*4);
+        if( relativeLength < 1.0/32.0 ) continue;
+        if( currentNoteDuration < shortestDuration or shortestDuration==-1) shortestDuration = currentNoteDuration;
+    }
+    assertExpr(lastNote,>,-1);
+    assertExpr(lastNote,<,noteAmount);
+    
+    if(measure_empty)
+    {
+        newTrackRef->firstNote = -1;
+        newTrackRef->lastNote = -1;
+    }
+    else newTrackRef->lastNote = lastNote; // ID of the last note in this measure
+    
+    //std::cout << "measure " << id << " empty=" << measure_empty << " from=" << newTrackRef->firstNote << " to=" << newTrackRef->lastNote << std::endl;
+    
+    // std::cout << "--- measure " << (id+1) << " ranges from note  "<< newTrackRef->firstNote << " to " << newTrackRef->lastNote << std::endl;
+    trackRef.push_back( newTrackRef );
+    
+    // check if all notes were used
+    if(lastNote == noteAmount-1) return -1;
+    
+    // if this measure is empty, return the same note as the one given in input (i.e. it was not used)
+    // if this measure is not empty, add 1 so next measure will start from the next
+    return lastNote + ( measure_empty ? 0 : 1);
+}
+
+#if 0
+#pragma mark -
+#endif
+
+// used to determine the order of what appears in the file.
+// the order is found first before writing anything because that allows more flexibility
+LayoutElement::LayoutElement(LayoutElementType type_arg, int measure_arg)
+{
+    type = type_arg;
+    measure = measure_arg;
+    
+    x = -1;
+    x2 = -1;
+}
+
+#if 0
+#pragma mark -
+#endif
+
+LayoutLine::LayoutLine(AriaPrintable* parent)
+{
+    printable = parent;
+    currentTrack = 0;
+    last_of_page = false;
+}
+
+int LayoutLine::getTrackAmount() const
+{
+    return printable->tracks.size();
+}
+void LayoutLine::setCurrentTrack(const int n)
+{
+    currentTrack = n;
+}
+Track* LayoutLine::getTrack() const
+{
+    assertExpr(currentTrack,>=,0);
+    assertExpr(currentTrack,<,printable->tracks.size());
+    return printable->tracks.get(currentTrack);
+}
+int LayoutLine::getFirstNoteInElement(const int layoutElementID)
+{
+    return getMeasureForElement(layoutElementID).trackRef[currentTrack].firstNote;
+}
+int LayoutLine::getLastNoteInElement(const int layoutElementID)
+{
+    std::cout << "last note in element " << layoutElementID << " of track " << currentTrack << " is " << getMeasureForElement(layoutElementID).trackRef[currentTrack].lastNote << " from measure " << getMeasureForElement(layoutElementID).id << std::endl;
+    return getMeasureForElement(layoutElementID).trackRef[currentTrack].lastNote;
+}
+int LayoutLine::getFirstNoteInElement(LayoutElement* layoutElement)
+{
+    return getMeasureForElement(layoutElement).trackRef[currentTrack].firstNote;
+}
+int LayoutLine::getLastNoteInElement(LayoutElement* layoutElement)
+{
+    return getMeasureForElement(layoutElement).trackRef[currentTrack].lastNote;
+}
+
+MeasureToExport& LayoutLine::getMeasureForElement(const int layoutElementID) const
+{
+    const int measID = layoutElements[layoutElementID].measure;
+    if(measID == -1) return (MeasureToExport&)nullMeasure;
+    return printable->measures[measID];
+}
+MeasureToExport& LayoutLine::getMeasureForElement(LayoutElement* layoutElement)
+{
+    return printable->measures[layoutElement->measure];
+}
+int LayoutLine::getLastMeasure() const
+{
+    for(int n=layoutElements.size()-1; n>=0; n--)
+    {
+        if( layoutElements[n].measure != -1) return layoutElements[n].measure;
+    }
+    return -1;
+}
+int LayoutLine::getFirstMeasure() const
+{
+    const int amount = layoutElements.size();
+    for(int n=0; n<amount; n++)
+    {
+        if( layoutElements[n].measure != -1) return layoutElements[n].measure;
+    }
+    return -1;
+}
+int LayoutLine::getLastNote() const
+{
+    const int track_amount = getTrackAmount();
+    const Track* t = getTrack();
+    
+    const int elements = layoutElements.size();
+    for(int el=elements-1; el>=0; el--)
+    { // start searching from last measure in this line
+        MeasureToExport& current_meas = getMeasureForElement(el);
+        for(int i=0; i<track_amount; i++)
+        {
+            if(current_meas.trackRef.size() > 0 && // FIXME - find why it's sometimes 0
+               current_meas.trackRef[i].track == t &&
+               current_meas.trackRef[i].lastNote != -1)
+            {
+                return current_meas.trackRef[i].lastNote;
+            }
+        }
+    }
+    return -1; // empty line
+}
+
+int LayoutLine::getFirstNote() const
+{
+    //const int measure = getFirstMeasure();
+    // const int from_tick = getMeasureData()->firstTickInMeasure(measure);
+    
+    const int track_amount = getTrackAmount();
+    
+    const Track* t = getTrack();
+    
+    const int elements = layoutElements.size();
+    for(int el=0; el<elements; el++)
+    { // start searching from first measure in this line
+        MeasureToExport& current_meas = getMeasureForElement(el);
+        for(int i=0; i<track_amount; i++)
+        {
+            if(current_meas.trackRef.size() > 0 && // FIXME - find why it's sometimes empty
+               current_meas.trackRef[i].track == t &&
+               current_meas.trackRef[i].firstNote != -1)
+                return current_meas.trackRef[i].firstNote;
+        }
+    }
+    return -1; // empty line
+    
+}
+int LayoutLine::calculateHeight()
+{
+    level_height = 0;
+    
+    std::vector<int> heights;
+    
+    /* calculate the total height of this line (which many include multiple tracks */
+    std::cout << "---- line ----" << std::endl;
+    const int trackAmount = getTrackAmount();
+    for(int n=0; n<trackAmount; n++)
+    {
+        setCurrentTrack(n);
+        const int this_height = printable->editorPrintables.get(currentTrack)->calculateHeight(*this);
+        heights.push_back(this_height);
+        level_height += this_height;
+        std::cout << this_height << "-high" << std::endl;
+    }
+    
+    /* distribute the vertical space between tracks (some track need more vertical space than others) */
+    for(int n=0; n<trackAmount; n++)
+    {
+        height_percent.push_back( (int)round( (float)heights[n] * 100.0f / (float)level_height ) );
+        std::cout << height_percent[n] << "%" << std::endl;
+    }
+    
+    // if we're the last of the page, we need less space cause we don't
+    // need to leave empty space under
+    if(last_of_page) level_height -= 13;
+    
+    return level_height;
+}
+
+// FIXME: this class manages layout, should not do any drawing
+void LayoutLine::printYourself(wxDC& dc, const int x0, const int y0, const int x1, const int y1,
+                               int margin_below, int margin_above)
+{
+    const int trackAmount = getTrackAmount();
+    
+    // ---- empty space around whole line
+    const float height = (float)(y1 - y0);// - ( trackAmount>1 and not last_of_page ? 100 : 0 );
+    
+    if(height < 0.0001) return; // empty line. TODO : todo - draw empty bars to show there's something?
+    
+    // make sure margins are within acceptable bounds
+    if(margin_below > height) margin_below = height/5;
+    if(margin_above > height) margin_above = height/5;
+    
+    const int my0 = y0 + margin_above;
+    const int my1 = y0 + height - margin_below;
+    
+    // draw vertical line to show these lines belong toghether
+    if(trackAmount>1)
+    {
+        dc.SetPen(  wxPen( wxColour(150,150,150), 25 ) );
+        dc.DrawLine( x0-3, my0, x0-3, my1); // vertical line
+        dc.DrawLine( x0-3, my0, x0-3+30, my0-50); // top thingy
+        dc.DrawLine( x0-3, my1, x0-3+30, my1+50); // bottom thingy
+        
+        dc.DrawLine( x1-3, my0, x1-3, my1); // right-side line
+    }
+    
+    // ---- space between individual tracks
+    const int space_between_tracks = 150;
+    
+    float current_y = my0;
+    for(int n=0; n<trackAmount; n++)
+    {
+        setCurrentTrack(n);
+        EditorPrintable* track = printable->editorPrintables.get(currentTrack);
+        
+        // skip empty tracks
+        if(height_percent[n] == 0) continue;
+        
+        
+        // determine how much vertical space is allocated for this track
+        const float track_height = (height - margin_below - margin_above) * height_percent[n]/100.0f;
+        
+        //std::cout << "* allocating " <<track_height << " out of " << height << " (" << height_percent[n] << "%)" << std::endl;
+        
+        const float position = (float)n / trackAmount;
+        const float space_above_line = space_between_tracks*position;
+        const float space_below_line = space_between_tracks*(1-position);
+        
+        track->drawLine(*this, dc,
+                        x0, current_y + space_above_line,
+                        x1, current_y + track_height - space_below_line,
+                        n==0);
+        current_y += track_height;
+        //assertExpr(current_y,<=,y1);
+    }
+}
+    
+}
+
---
+++ Printing/LayoutTree.h
@@ -0,0 +1,194 @@
+/*
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _layout_tree_h_
+#define _layout_tree_h_
+
+#include <map>
+#include "ptr_vector.h"
+
+namespace AriaMaestosa
+{
+    class Track;
+    class AriaPrintable;
+    
+    /*
+     A description of a measure to print. If we print more than one track at once,
+     each measure will hold multiple 'MeasureTrackReference' instances.
+     A 'MeasureTrackReference' "ties" a MeasureToExport to a Track object,
+     by keeping a pointer to it and holding the range of notes in that track
+     that belong to this measure.
+     */
+    
+    class MeasureTrackReference
+        {
+        public:
+            Track* track;
+            int firstNote, lastNote;
+        };
+    
+    class MeasureToExport
+        {
+        public:
+            MeasureToExport(const int measID);
+            
+            // for non-linear printing
+            std::map< int /* tick */, float /* position */ > ticks_relative_position;
+            
+            // Finds the notes correcsponding to this measure
+            // in the given track and keep the reference.
+            // Returns the ID of the last note in this measure
+            int addTrackReference(const int firstNote, Track* track);
+            
+            // used when we print more than one track
+            // each track we print will have one entry here
+            // for each printed measure
+            ptr_vector<MeasureTrackReference> trackRef;
+            
+            // first and last tick in this measure
+            int firstTick, lastTick;
+            
+            // if this measure is later repeated and is not a repetition of a previous measure,
+            // contains ID of all later measures similar to this one
+            std::vector<int> similarMeasuresFoundLater;
+            
+            // if this measure is a repetition of a previous measure, contains the ID of which one
+            int firstSimilarMeasure;
+            
+            // shortest note in the measure (will be used to
+            // determine "zoom" on measure. e.g. a measure with very
+            // short notes takes more room).
+            int shortestDuration;
+            
+            // ID of the measure
+            int id;
+            
+            // true if measure needs to be apart from others
+            // mostly used with repetitions (e.g. x4) to tell where the repetition starts
+            // FIXME - doesn't really belong here, should be a layout element
+            bool cutApart;
+            
+            bool calculateIfMeasureIsSameAs(MeasureToExport& checkMeasure);
+            
+            // if a repetition is found, it is stored in the variables and returns true,
+            // otherwise returns false
+            bool findConsecutiveRepetition(ptr_vector<MeasureToExport>& measures, const int measureAmount,
+                                           int& firstMeasureThatRepeats /*out*/, int& lastMeasureThatRepeats /*out*/,
+                                           int& firstMeasureRepeated /*out*/, int& lastMeasureRepeated /*out*/);
+        };
+    
+    extern const MeasureToExport nullMeasure;
+    
+    // used to determine the order of what appears in the file.
+    // the order is found first before writing anything because that allows more flexibility
+    enum LayoutElementType
+    {
+        SINGLE_MEASURE,
+        SINGLE_REPEATED_MEASURE,
+        EMPTY_MEASURE,
+        REPEATED_RIFF,
+        PLAY_MANY_TIMES,
+        LINE_HEADER, // like the clef and key (on a score) or the word TAB and the tuning (for tabs)
+        TIME_SIGNATURE
+    };
+    class LayoutElement
+        {
+        public:
+            LayoutElement(LayoutElementType type_arg, int measure_arg = -1);
+            
+            LayoutElementType type;
+            
+            int measure;
+            
+            // used in many-measure repetitions. the first 2 ones are the measures that repeat, the last 2 ones the measures being repeated
+            int firstMeasure, lastMeasure, firstMeasureToRepeat, lastMeasureToRepeat;
+            
+            // used for time sig elements
+            int num, denom;
+            
+            int amountOfTimes; // used for 'play many times' events
+            
+            //float zoom;
+            int width_in_units;
+            
+            // filled by EditorPrintable::continueWithNextElement() as the X position is calculated in
+            // case location is needed later (for instance, if more than one rendering pass)
+            int x, x2;
+        };
+    
+    class EditorData
+        {
+        public:
+            virtual ~EditorData() {}
+        };
+    
+    /*
+     A line on a notation to print. Can contain more than one track.
+     Essentially holds some 'LayoutElement' objects (the ones that fit
+     on this line)
+     */
+    class LayoutLine
+        {
+            int currentTrack;
+            AriaPrintable* printable;
+            
+            /** used to store what percentage of this line's height this track should take.
+             e.g. a score with F+G clefs will need more space than a 4-string bass tab
+             so vertical space must not be divided equally */
+            std::vector<short int> height_percent;
+        public:
+            LayoutLine(AriaPrintable* parent);
+            
+            // editors can put data of their own there.
+            OwnerPtr<EditorData> editor_data;
+            
+            int width_in_units;
+            int level_height;
+            
+            bool last_of_page;
+            
+            int getTrackAmount() const;
+            void setCurrentTrack(const int n);
+            Track* getTrack() const;
+            int getFirstNoteInElement(const int layoutElementID);
+            int getLastNoteInElement(const int layoutElementID);
+            int getFirstNoteInElement(LayoutElement* layoutElement);
+            int getLastNoteInElement(LayoutElement* layoutElement);
+            
+            void printYourself(wxDC& dc, const int x0, const int y0, const int x1, const int y1, int margin_below, int margin_above);
+            int calculateHeight();
+            
+            MeasureToExport& getMeasureForElement(const int layoutElementID) const;
+            MeasureToExport& getMeasureForElement(LayoutElement* layoutElement);
+            
+            int getLastMeasure() const;
+            int getFirstMeasure() const;
+            
+            int getLastNote() const;
+            int getFirstNote() const;
+            
+            std::vector<LayoutElement> layoutElements;
+        };
+    
+    struct LayoutPage
+    {
+        int first_line;
+        int last_line;
+    };
+    
+}
+
+#endif
