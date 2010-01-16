92be2694b0340cd8668387b9dc1fa2de772cccdb
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2010-01-16 18:33:48 +0000

    Fixed bug when opening OpenGL images

--- trunk/Renderers/GLImage.cpp
+++ trunk/Renderers/GLImage.cpp
@@ -52,9 +52,13 @@ namespace AriaMaestosa {
             }
         }
 
-        wxImage* img = new wxImage( getResourcePrefix()  + path );
+        wxImage* img = new wxImage( getResourcePrefix() + path );
 
-        (*imageWidth) = img->GetWidth();
+        //std::cout << path.mb_str() << (img->HasAlpha() ? " has alpha" : " does NOT have alpha")
+        //          << " and " << (img->HasMask() ? " DOES have a mask" : " does note have a mask") << std::endl;
+
+        
+        (*imageWidth)  = img->GetWidth();
         (*imageHeight) = img->GetHeight();
 
         glPixelStorei(GL_UNPACK_ALIGNMENT,   1   );
@@ -62,97 +66,84 @@ namespace AriaMaestosa {
         float power_of_two_that_gives_correct_width=std::log((float)(*imageWidth))/std::log(2.0);
         float power_of_two_that_gives_correct_height=std::log((float)(*imageHeight))/std::log(2.0);
 
-        // check if image bounds are a power of two
-        if ( (int)power_of_two_that_gives_correct_width == power_of_two_that_gives_correct_width and
-            (int)power_of_two_that_gives_correct_height == power_of_two_that_gives_correct_height)
+
+        // If the image is not a power-of-two, we need to copy it over to a larger power-of-2 image.
+        // Also, pack alpha/masks into the bytes to be RGBA (wx provides BGR + A separately)
+        int newWidth  = (int)std::pow( 2.0, (int)(std::ceil(power_of_two_that_gives_correct_width)) );
+        int newHeight = (int)std::pow( 2.0, (int)(std::ceil(power_of_two_that_gives_correct_height)) );
+
+        GLubyte* bitmapData = img->GetData();
+        GLubyte* alphaData  = (img->HasAlpha() ? img->GetAlpha() : NULL);
+        GLubyte* imageData;
+
+        int old_bytesPerPixel = 3;
+        int bytesPerPixel = img->HasAlpha() or img->HasMask() ?  4 : 3;
+
+        //std::cout << "   bytesPerPixel = " << bytesPerPixel << "\n";
+        
+        int imageSize = newWidth * newHeight * bytesPerPixel;
+        imageData = (GLubyte *)malloc(imageSize);
+
+        int rev_val = (*imageHeight)-1;
+
+        for (int y=0; y<newHeight; y++)
         {
-
-            // if yes, everything is fine
-
-            glTexImage2D(GL_TEXTURE_2D,
-                         0,
-                         img->HasAlpha() ?  4 : 3,
-                         *imageWidth,
-                         *imageHeight,
-                         0,
-                         // here's where we have the format of the item which is RGB
-                         img->HasAlpha() ?  GL_RGBA : GL_RGB,
-                         GL_UNSIGNED_BYTE,
-                         img->GetData());
-
-            (*textureWidth)  = (*imageWidth);
-            // hack, textureHeight made smaller than zero when image was power of two.
-            // in these cases, the image will be upside down so we need to flip it
-            (*textureHeight) = -(*imageHeight);
-
-        }
-        else // ----------------- Texture is not a power of two. We need to resize it --------------------
-        {
-
-            int newWidth=(int)std::pow( 2.0, (int)(std::ceil(power_of_two_that_gives_correct_width)) );
-            int newHeight=(int)std::pow( 2.0, (int)(std::ceil(power_of_two_that_gives_correct_height)) );
-
-            GLubyte    *bitmapData = img->GetData();
-            GLubyte    *alphaData = img->GetAlpha();
-            GLubyte    *imageData;
-
-            int old_bytesPerPixel = 3;
-            int bytesPerPixel = img->HasAlpha() ?  4 : 3;
-
-            int imageSize = newWidth * newHeight * bytesPerPixel;
-            imageData=(GLubyte *)malloc(imageSize);
-
-            int rev_val=(*imageHeight)-1;
-
-            for(int y=0; y<newHeight; y++)
+            for (int x=0; x<newWidth; x++)
             {
-                for(int x=0; x<newWidth; x++)
+                const int destArrayIndex = (x+y*newWidth)*bytesPerPixel;
+                
+                if (x<(*imageWidth) and y<(*imageHeight))
                 {
+                    const int srcArrayIndex  = (x+(rev_val-y)*(*imageWidth))*old_bytesPerPixel;
+                    assertExpr( srcArrayIndex + (bytesPerPixel-1), <, imageSize);
 
-                    if ( x<(*imageWidth) && y<(*imageHeight) ){
-                        assertExpr( (x+y*newWidth)*bytesPerPixel+ (bytesPerPixel-1), <, imageSize);
+                    imageData[destArrayIndex+0] = bitmapData[srcArrayIndex + 0];
+                    imageData[destArrayIndex+1] = bitmapData[srcArrayIndex + 1];
+                    imageData[destArrayIndex+2] = bitmapData[srcArrayIndex + 2];
 
-                        imageData[(x+y*newWidth)*bytesPerPixel+0]=
-                            bitmapData[( x+(rev_val-y)*(*imageWidth))*old_bytesPerPixel + 0];
-
-                        imageData[(x+y*newWidth)*bytesPerPixel+1]=
-                            bitmapData[( x+(rev_val-y)*(*imageWidth))*old_bytesPerPixel + 1];
-
-                        imageData[(x+y*newWidth)*bytesPerPixel+2]=
-                            bitmapData[( x+(rev_val-y)*(*imageWidth))*old_bytesPerPixel + 2];
-
-                        if (bytesPerPixel==4) imageData[(x+y*newWidth)*bytesPerPixel+3]=
-                            alphaData[ x+(rev_val-y)*(*imageWidth) ];
-
-                    }
-                    else
+                    if (bytesPerPixel==4)
                     {
-                        assertExpr( (x+y*newWidth)*bytesPerPixel+(bytesPerPixel-1), <, imageSize);
-
-                        imageData[(x+y*newWidth)*bytesPerPixel+0] = 0;
-                        imageData[(x+y*newWidth)*bytesPerPixel+1] = 0;
-                        imageData[(x+y*newWidth)*bytesPerPixel+2] = 0;
-                        if (bytesPerPixel==4) imageData[(x+y*newWidth)*bytesPerPixel+3] = 0;
+                        if (img->HasMask())
+                        {
+                            imageData[destArrayIndex+3] = (img->IsTransparent(x, y) ? 0 : 255);
+                            //std::cout << "Pixel " << x << ", " << y << " has mask value " << imageData[destArrayIndex+3] << std::endl;
+                        }
+                        else
+                        {
+                            assert(alphaData != NULL);
+                            imageData[destArrayIndex+3] = alphaData[ x+(rev_val-y)*(*imageWidth) ];
+                        }
                     }
 
-                }//next
+                }
+                else
+                {
+                    assertExpr( (x+y*newWidth)*bytesPerPixel+(bytesPerPixel-1), <, imageSize);
+
+                    imageData[destArrayIndex+0] = 0;
+                    imageData[destArrayIndex+1] = 0;
+                    imageData[destArrayIndex+2] = 0;
+                    if (bytesPerPixel==4) imageData[destArrayIndex+3] = 0;
+                }
+
             }//next
+        }//next
 
-            glTexImage2D(GL_TEXTURE_2D,
-                         0,
-                         img->HasAlpha() ?  4 : 3,
-                         newWidth,
-                         newHeight,
-                         0,
-                         img->HasAlpha() ?  GL_RGBA : GL_RGB,
-                         GL_UNSIGNED_BYTE,
-                         imageData);
+        glTexImage2D(GL_TEXTURE_2D,
+                     0,
+                     (img->HasAlpha() or img->HasMask() ?  4 : 3),
+                     newWidth,
+                     newHeight,
+                     0,
+                     (img->HasAlpha() or img->HasMask() ?  GL_RGBA : GL_RGB),
+                     GL_UNSIGNED_BYTE,
+                     imageData);
 
-            (*textureWidth)=newWidth;
-            (*textureHeight)=newHeight;
+        (*textureWidth)=newWidth;
+        (*textureHeight)=newHeight;
 
-            free(imageData);
-        }
+        free(imageData);
+
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // GL_LINEAR
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); // GL_LINEAR
