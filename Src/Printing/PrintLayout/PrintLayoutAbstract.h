40a576bfac0c1f78fe6632c71ae3e1e60c90e332
auria <48419909+mg186@users.noreply.github.com>
2010-03-17 20:48:56 +0000

    Code cleanup, documentation

--- Src/Printing/PrintLayout/PrintLayoutAbstract.h
+++ Src/Printing/PrintLayout/PrintLayoutAbstract.h
@@ -49,19 +49,20 @@ namespace AriaMaestosa
     class PrintLayoutAbstract
     {
         /** Reference to the parent sequence */
-        PrintableSequence* sequence;
+        PrintableSequence* m_sequence;
         
-        /** referencing vectors from PrintableSequence (FIXME? no two objects should reference these vectors) */
-        ptr_vector<LayoutPage>& m_layout_page;
-        
-        ptr_vector<PrintLayoutMeasure> measures; 
+        /** holds all measure objects from this sequence (there are not multiple copies of these
+          * objects for each measure number even if multiple tracks are being printed) 
+          */
+        ptr_vector<PrintLayoutMeasure> m_measures; 
         
 
         /**
           * @brief Builds the Page/Line layout tree from the full list of layout elements
-          * Creates LayoutPage and LayoutLine objects, add the created layout elements to their corresponding line
+          * Creates LayoutPage and LayoutLine objects, add the created layout elements to their
+          * corresponding line
           */
-        void layInLinesAndPages(std::vector<LayoutElement>& layoutElements);
+        void layInLinesAndPages(std::vector<LayoutElement>& layoutElements, ptr_vector<LayoutPage>& layoutPages);
         
         /** The main goal of this method is to set the 'width_in_print_units' member of each LayoutElement */
         void calculateRelativeLengths(std::vector<LayoutElement>& layoutElements);
@@ -74,20 +75,42 @@ namespace AriaMaestosa
           */
         void createLayoutElements(std::vector<LayoutElement>& layoutElements, bool checkRepetitions_bool);
         
+        /** fills fields containing info about similar measures withing the PrintLayoutMeasure objects */
         void findSimilarMeasures();
         
         /** utility method invoked by 'layInLinesAndPages' when a line is complete */
-        void terminateLine(LayoutLine* line, const int maxLevelHeight,
+        void terminateLine(LayoutLine* line, ptr_vector<LayoutPage>& layoutPages, const int maxLevelHeight,
                            int& current_height, int& current_page);
 
-    public:
-        PrintLayoutAbstract(PrintableSequence* parent,
-                           ptr_vector<LayoutPage>& m_layout_page  /* out */);
-        
+        /** generates measures. needs to be called before "calculateLayoutElements" */
         void generateMeasures(ptr_vector<Track, REF>& tracks);
         
-        /** main function called from other classes. measures must have been generated. */
-        void calculateLayoutElements(ptr_vector<Track, REF>& track, const bool checkRepetitions_bool);
+        /**
+          * @precondition measures must have been generated.
+          *
+          * @param[out] layoutPages
+          */
+        void calculateLayoutElements(ptr_vector<Track, REF>& tracks,
+                                     ptr_vector<LayoutPage>& layoutPages,
+                                     const bool checkRepetitions_bool);
+        
+    public:
+        /**
+          * Constructs a PrintLayoutAbstract object that can lay a given sequence within pages
+          * @param sequence   which sequence this layout manager will analyse and fit
+          */
+        PrintLayoutAbstract(PrintableSequence* sequence);
+        
+        /**
+          * @brief                  main function called from other classes
+          * @precondition           measures must have been generated.
+          * @param tracks           a list of all tracks to be printed.
+          * @param[out] layoutPages the vector of pages that is filled by this call
+          * @param findReps         whether to use the feature to automatically attempt to detect
+          *                         repeated song sections
+          */
+        void addLayoutInformation(ptr_vector<Track, REF>& tracks, ptr_vector<LayoutPage>& layoutPages,
+                                  const bool findReps);
     };
     
 }
