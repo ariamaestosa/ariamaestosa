22fea256f4f614a9b1dbe609fcfed00878065129
auria <48419909+mg186@users.noreply.github.com>
2010-04-16 13:54:16 +0000

    Don't use     SetBackgroundStyle(wxBG_STYLE_PAINT);

--- Src/Renderers/wxRenderPane.cpp
+++ Src/Renderers/wxRenderPane.cpp
@@ -36,7 +36,11 @@ using namespace AriaMaestosa;
 wxRenderPane::wxRenderPane(MainFrame* mainFrame, int* args) :
     wxPanel(mainFrame, wxID_ANY,  wxDefaultPosition, wxDefaultSize)
 {
+#if wxCHECK_VERSION(2,9,1)
     SetBackgroundStyle(wxBG_STYLE_PAINT);
+#else
+    SetBackgroundStyle(wxBG_STYLE_CUSTOM);
+#endif
 }
 
 // ----------------------------------------------------------------------------------------------------------
