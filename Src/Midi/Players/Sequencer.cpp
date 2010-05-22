05b6ec00ef971f41f5efd0a31e5f8b36f1b3633c
auria <48419909+mg186@users.noreply.github.com>
2010-05-22 19:55:56 +0000

    minor code style improvements

--- Src/Midi/Players/Sequencer.cpp
+++ Src/Midi/Players/Sequencer.cpp
@@ -93,7 +93,7 @@ BasicTimer* timer = NULL;
 
 void cleanup_sequencer()
 {
-    if(timer != NULL) delete timer;
+    if (timer != NULL) delete timer;
     timer = NULL;
 }
 
@@ -119,7 +119,7 @@ void AriaSequenceTimer::run(jdkmidi::MIDISequencer* jdksequencer, const int song
     int ev_track;
 
     jdkmidi::MIDIClockTime tick;
-    if(!jdksequencer->GetNextEventTime(&tick))
+    if (not jdksequencer->GetNextEventTime(&tick))
     {
         std::cout << "failed to get first event time, returning" << std::endl;
         cleanup_sequencer();
@@ -144,7 +144,7 @@ void AriaSequenceTimer::run(jdkmidi::MIDISequencer* jdksequencer, const int song
         // process all events that need to be done by the current tick
         while( next_event_time <= total_millis )
         {
-            if(!jdksequencer->GetNextEvent( &ev_track, &ev ))
+            if (not jdksequencer->GetNextEvent( &ev_track, &ev ))
             {
                 // error
                 std::cerr << "error, failed to retrieve next event, returning" << std::endl;
@@ -153,54 +153,54 @@ void AriaSequenceTimer::run(jdkmidi::MIDISequencer* jdksequencer, const int song
             }
             const int channel = ev.GetChannel();
 
-            if( ev.IsNoteOn() )
+            if ( ev.IsNoteOn() )
             {
                 const int note = ev.GetNote();
                 const int volume = ev.GetVelocity();
                 PlatformMidiManager::seq_note_on(note, volume, channel);
             }
-            else if( ev.IsNoteOff() )
+            else if ( ev.IsNoteOff() )
             {
                 const int note = ev.GetNote();
                 PlatformMidiManager::seq_note_off(note, channel);
             }
-            else if( ev.IsControlChange() )
+            else if ( ev.IsControlChange() )
             {
                 const int controllerID = ev.GetController();
                 const int value = ev.GetControllerValue();
                 PlatformMidiManager::seq_controlchange(controllerID, value, channel);
             }
-            else if( ev.IsPitchBend() )
+            else if ( ev.IsPitchBend() )
             {
                 const int pitchBendVal = ev.GetBenderValue();
                 PlatformMidiManager::seq_pitch_bend(pitchBendVal, channel);
             }
-            else if( ev.IsProgramChange() )
+            else if ( ev.IsProgramChange() )
             {
                 const int instrument = ev.GetPGValue();
                 PlatformMidiManager::seq_prog_change(instrument, channel);
             }
-            else if( ev.IsTempo() )
+            else if ( ev.IsTempo() )
             {
                 //std::cout << "tempo event" << std::endl;
                 const int bpm = ev.GetTempo32()/32;
                 ticks_per_millis = (double)bpm * (double)beatlen / (double)60000.0;
             }
             /*
-            else if( ev.IsPolyPressure() )
+            else if ( ev.IsPolyPressure() )
                 std::cout << "poly pressure" << std::endl;
-            else if( ev.IsChannelPressure() )
+            else if ( ev.IsChannelPressure() )
                 std::cout << "channel pressure" << std::endl;
-            else if( ev.IsSystemMessage() )
+            else if ( ev.IsSystemMessage() )
                 std::cout << "system message " << std::hex << ev.GetType() << std::endl;
 
-            if( ev.IsSysEx() )
+            if ( ev.IsSysEx() )
                 std::cout << "sys ex " << ev.GetSysExNum() << std::endl;
-            else if( ev.IsSongPosition() )
+            else if ( ev.IsSongPosition() )
                 std::cout << "song position" << std::endl;
-            else if( ev.IsNoOp() )
+            else if ( ev.IsNoOp() )
                 std::cout << "no op" << std::endl;
-            else if( ev.IsBeatMarker() )
+            else if ( ev.IsBeatMarker() )
                 std::cout << "beat marker" << std::endl;
             else
             {
@@ -209,14 +209,14 @@ void AriaSequenceTimer::run(jdkmidi::MIDISequencer* jdksequencer, const int song
 
             previous_tick = tick;
 
-            if(!jdksequencer->GetNextEventTime(&tick))
+            if (not jdksequencer->GetNextEventTime(&tick))
             {
                 cleanup_sequencer();
                 return;
             }
-            if((long)tick <(long) previous_tick) continue; // something wrong about time order...
+            if ((long)tick <(long) previous_tick) continue; // something wrong about time order...
 
-            if((long)tick > (long)songLengthInTicks)
+            if ((long)tick > (long)songLengthInTicks)
             {
                 std::cout << "done, thread will exit" << std::endl;
                 PlatformMidiManager::seq_notify_current_tick(-1);
@@ -233,8 +233,8 @@ void AriaSequenceTimer::run(jdkmidi::MIDISequencer* jdksequencer, const int song
 
             /*
             static int i = 0;
-            i++; if(i>10) i=0;
-            if(i == 1)
+            i++; if (i>10) i=0;
+            if (i == 1)
             {
                 std::cout << "next_event_time = " << next_event_time << "  -> tick = " << tick << std::endl;
             }
