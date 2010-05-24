0df64d7d66bd360981fec6ab4956bdc4836f3a70
auria <48419909+mg186@users.noreply.github.com>
2010-05-24 20:51:23 +0000

    Added initial Freedesktop support (.desktop file, file associations, etc...)

---
+++ Freedesktop/installMimeType.sh
@@ -0,0 +1,13 @@
+# This is an attempt at installing Aria Maestosa in a Freedesktop-compliant way, with a .desktop file and correct file associations.
+# At the moment, file associations "sort of" work (the file icon does not show; opening works but if you open multiple files, multiple
+# Aria processes will be launched).
+# This script will also install Aria in /usr/share/applications (path not configurable for now, sorry)
+
+# copies the icon file into '~/.local/share/icons/hicolor/64x64/mimetypes'
+xdg-icon-resource install --context mimetypes --size 64 ./aria_doc.png audio-auria-ariamaestosa
+
+# this copies the file into '~/.local/share/mime/packages'
+xdg-mime install ./auria-ariamaestosa.xml
+
+sudo install ./Aria.desktop /usr/share/applications
+sudo update-desktop-database /usr/share/applications
