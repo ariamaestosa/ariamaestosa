aee7c8a6b2644cf8dcdab43897c29e9cb3d29491
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2010-06-19 00:32:55 +0000

    Improved unit test mini-framework

--- trunk/Src/unit_test.cpp
+++ trunk/Src/unit_test.cpp
@@ -2,22 +2,78 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include <map>
+#include "wx/string.h"
+#include "wx/tokenzr.h"
+#include "wx/filename.h"
 
 namespace TestCaseList
 {
-    std::vector<UnitTestCase*>* all_test_cases = NULL;
+    struct Node
+    {
+        std::string m_name;
+        std::vector<UnitTestCase*> m_test_cases;
+        std::map<wxString, Node> m_children;
+        
+        Node()
+        {
+        }
+        Node(const char* name)
+        {
+            m_name = name;
+        }
+    };
+    
+    Node* root = NULL;
+    
+    void add(UnitTestCase* testCase, const std::vector<wxString>& path)
+    {
+        if (root == NULL)
+        {
+            root = new Node("All");
+        }
+        
+        Node* currNode = root;
+        for (unsigned int n=0; n<path.size(); n++)
+        {
+            if (currNode->m_children.find(path[n]) == currNode->m_children.end())
+            {
+                currNode->m_children[path[n]] = Node(path[n]);
+            }
+            currNode = &currNode->m_children[path[n]];
+        }
+        currNode->m_test_cases.push_back(testCase);
+    }
+    
+    std::map<int, Node*> testGroupsById;
+    std::map<int, UnitTestCase*> testCasesById;
+
 }
 
-UnitTestCase::UnitTestCase(const char* name)
+UnitTestCase::UnitTestCase(const char* name, const char* filePath)
 {
-    if (TestCaseList::all_test_cases == NULL) TestCaseList::all_test_cases = new std::vector<UnitTestCase*>();
+    //if (TestCaseList::all_test_cases == NULL) TestCaseList::all_test_cases = new std::vector<UnitTestCase*>();
     
     m_name = name;
-    TestCaseList::all_test_cases->push_back(this);
+    
+    
+    std::vector<wxString> path;
+    wxStringTokenizer tokenizer(wxString(filePath).BeforeLast(wxFileName::GetPathSeparator()),
+                                wxFileName::GetPathSeparator());
+    while (tokenizer.HasMoreTokens())
+    {
+        wxString token = tokenizer.GetNextToken();
+        path.push_back(token);
+    }
+    
+    
+    TestCaseList::add(this, path);
 }
 
 UnitTestCase::~UnitTestCase()
 {
+    // TODO: cleanup!
+    /*
     for (unsigned int n=0; n<TestCaseList::all_test_cases->size(); n++)
     {
         if ((*TestCaseList::all_test_cases)[n] == this)
@@ -28,6 +84,7 @@ UnitTestCase::~UnitTestCase()
     }
     
     if (TestCaseList::all_test_cases->size() == 0) delete TestCaseList::all_test_cases;
+     */
 }
 
 void runTest(UnitTestCase* test)
@@ -50,16 +107,68 @@ void runTest(UnitTestCase* test)
     if (passed) std::cout << "passed" << std::endl;    
 }
 
-void UnitTestCase::showMenu()
+static int id = 1;
+
+void printTree(TestCaseList::Node* currNode, int ident)
 {
-    std::cout << "==== UNIT TESTS ===\n";
-    std::cout << "Make a choice : \n";
-    std::cout << " (0) Run all\n";
-    for (unsigned int n=0; n<TestCaseList::all_test_cases->size(); n++)
+    //for (int n=0; n<ident; n++) std::cout << "    ";
+    //std::cout << "(0) " << currNode->m_name << std::endl;
+    
+    
+    for (std::map<wxString, TestCaseList::Node>::iterator it = currNode->m_children.begin();
+         it != currNode->m_children.end();
+         it++)
     {
-        std::cout << " (" << (n+1) << ") " << (*TestCaseList::all_test_cases)[n]->m_name << "\n";
+        for (int i=0; i<=ident; i++) std::cout << "    ";
+        std::cout << "(" << id << ") [group] " << (*it).second.m_name << std::endl;
+        TestCaseList::testGroupsById[id] = &(*it).second;
+        id++;
+        
+        printTree(&((*it).second), ident+1);
     }
     
+    for (unsigned int n=0; n<currNode->m_test_cases.size(); n++)
+    {
+        for (int i=0; i<=ident; i++) std::cout << "    ";
+        std::cout << "(" << id << ") [test] " << currNode->m_test_cases[n]->getName() << std::endl;
+        TestCaseList::testCasesById[id] = currNode->m_test_cases[n];
+        id++;
+    }
+}
+
+void runTestsIn(TestCaseList::Node* node)
+{
+    for (unsigned int n=0; n<node->m_test_cases.size(); n++)
+    {
+        runTest(node->m_test_cases[n]);
+    }
+    
+    for (std::map<wxString, TestCaseList::Node>::iterator it = node->m_children.begin();
+         it != node->m_children.end();
+         it++)
+    {
+        runTestsIn(&(*it).second);
+    }
+}
+
+void UnitTestCase::showMenu()
+{
+    TestCaseList::testCasesById.clear();
+    TestCaseList::testGroupsById.clear();
+    id = 1;
+    
+    std::cout << "(0) [group] All Tests\n";
+    TestCaseList::Node* from = TestCaseList::root;
+    while (from->m_children.size() == 1)
+    {
+        from = &(from->m_children.begin()->second);
+    }
+    
+    std::cout << "==== UNIT TESTS ===\n";
+    printTree(from, 0);
+    
+    std::cout << "----\n";
+    std::cout << "Make a choice : \n";
     std::cout << "\n> ";
     fflush(stdout);
     
@@ -68,14 +177,19 @@ void UnitTestCase::showMenu()
     
     if (choice == 0)
     {
-        for (unsigned int n=0; n<TestCaseList::all_test_cases->size(); n++)
-        {
-            runTest( (*TestCaseList::all_test_cases)[n] );
-        }
+        runTestsIn(TestCaseList::root);
+    }
+    else if (TestCaseList::testGroupsById.find(choice) != TestCaseList::testGroupsById.end())
+    {
+        runTestsIn(TestCaseList::testGroupsById[choice]);
+    }
+    else if (TestCaseList::testCasesById.find(choice) != TestCaseList::testCasesById.end())
+    {
+        runTest(TestCaseList::testCasesById[choice]);
     }
     else
     {
-        runTest( (*TestCaseList::all_test_cases)[choice - 1] ); 
+        std::cerr << "Invalid input\n";
     }
 }
 
