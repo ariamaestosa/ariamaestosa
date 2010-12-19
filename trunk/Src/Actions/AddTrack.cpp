9378eb072aec119d34d8f05491ca4793ae18f9cc
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2010-12-19 16:40:31 +0000

    Applied patch by ln- to correct error undetected due to GCC bug. Thanks\!

--- trunk/Src/Actions/AddTrack.cpp
+++ trunk/Src/Actions/AddTrack.cpp
@@ -25,6 +25,7 @@
 #include "UnitTest.h"
 #include "UnitTestUtils.h"
 
+using namespace AriaMaestosa;
 using namespace AriaMaestosa::Action;
 
 // --------------------------------------------------------------------------------------------------------
