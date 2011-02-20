d80b79a99c00cdcd5a024077c067b6557bd68800
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2011-02-20 00:18:37 +0000

    remove uneeded FIXME

--- trunk/Src/IO/MidiToMemoryStream.h
+++ trunk/Src/IO/MidiToMemoryStream.h
@@ -28,8 +28,6 @@ namespace AriaMaestosa
     /**
      * libjdkmidi by default can only save midi bytes to a file.
      * So i wrote this "fake stream" that captures the bytes and stores them in memory rather than to a file.
-     * This is a quick'n'dirty hack that should probably be reconsidered, even though it doesn't seem to
-     * cause major performance issues. (FIXME)
      * @ingroup io
      */
     class MidiToMemoryStream : public jdkmidi::MIDIFileWriteStream
