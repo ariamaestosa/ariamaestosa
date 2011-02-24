28caa33642278e131bfc3bd485ca189c9b18073e
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2011-02-24 22:08:14 +0000

    Added the necessary code to detect external midi output devices (OSX)

--- trunk/Src/Midi/Players/Mac/AUNotePlayer.cpp
+++ trunk/Src/Midi/Players/Mac/AUNotePlayer.cpp
@@ -24,7 +24,7 @@
 #ifdef _MAC_QUICKTIME_COREAUDIO
 #include "Utils.h"
 
-#include "Midi/Players/Mac/CoreAudioNotePlayer.h"
+#include "Midi/Players/Mac/AUNotePlayer.h"
 
 #include <CoreServices/CoreServices.h> //for file stuff
 #include <AudioUnit/AudioUnit.h>
@@ -59,10 +59,137 @@ public:
 // ==========================================================================================
 // ==========================================================================================
 
-
+// The following is copied from: http://developer.apple.com/qa/qa2004/qa1374.html
+static CFStringRef EndpointName(MIDIEndpointRef endpoint, bool isExternal)
+{
+    CFMutableStringRef result = CFStringCreateMutable(NULL, 0);
+    CFStringRef str;
+    
+    // begin with the endpoint's name
+    str = NULL;
+    MIDIObjectGetStringProperty(endpoint, kMIDIPropertyName, &str);
+    if (str != NULL) {
+        CFStringAppend(result, str);
+        CFRelease(str);
+    }
+    
+    MIDIEntityRef entity = NULL;
+    MIDIEndpointGetEntity(endpoint, &entity);
+    if (entity == NULL)
+        // probably virtual
+        return result;
+    
+    if (CFStringGetLength(result) == 0) {
+        // endpoint name has zero length -- try the entity
+        str = NULL;
+        MIDIObjectGetStringProperty(entity, kMIDIPropertyName, &str);
+        if (str != NULL) {
+            CFStringAppend(result, str);
+            CFRelease(str);
+        }
+    }
+    // now consider the device's name
+    MIDIDeviceRef device = NULL;
+    MIDIEntityGetDevice(entity, &device);
+    if (device == NULL)
+        return result;
+    
+    str = NULL;
+    MIDIObjectGetStringProperty(device, kMIDIPropertyName, &str);
+    if (str != NULL) {
+        // if an external device has only one entity, throw away
+        // the endpoint name and just use the device name
+        if (isExternal && MIDIDeviceGetNumberOfEntities(device) < 2) {
+            CFRelease(result);
+            return str;
+        } else {
+            // does the entity name already start with the device name?
+            // (some drivers do this though they shouldn't)
+            // if so, do not prepend
+            if (CFStringCompareWithOptions(str /* device name */,
+                                           result /* endpoint name */,
+                                           CFRangeMake(0, CFStringGetLength(str)), 0) != kCFCompareEqualTo) {
+                // prepend the device name to the entity name
+                if (CFStringGetLength(result) > 0)
+                    CFStringInsert(result, 0, CFSTR(" "));
+                CFStringInsert(result, 0, str);
+            }
+            CFRelease(str);
+        }
+    }
+    return result;
+}
+static CFStringRef ConnectedEndpointName(MIDIEndpointRef endpoint)
+{
+    CFMutableStringRef result = CFStringCreateMutable(NULL, 0);
+    CFStringRef str;
+    OSStatus err;
+    int i = 0;
+    
+    // Does the endpoint have connections?
+    CFDataRef connections = NULL;
+    int nConnected = 0;
+    bool anyStrings = false;
+    err = MIDIObjectGetDataProperty(endpoint, kMIDIPropertyConnectionUniqueID, &connections);
+    if (connections != NULL) {
+        // It has connections, follow them
+        // Concatenate the names of all connected devices
+        nConnected = CFDataGetLength(connections) / sizeof(MIDIUniqueID);
+        if (nConnected) {
+            const SInt32 *pid = (SInt32*) CFDataGetBytePtr(connections);
+            for (i = 0; i < nConnected; ++i, ++pid) {
+                MIDIUniqueID id = EndianS32_BtoN(*pid);
+                MIDIObjectRef connObject;
+                MIDIObjectType connObjectType;
+                err = MIDIObjectFindByUniqueID(id, &connObject, &connObjectType);
+                if (err == noErr) {
+                    if (connObjectType == kMIDIObjectType_ExternalSource  ||
+                        connObjectType == kMIDIObjectType_ExternalDestination) {
+                        // Connected to an external device's endpoint (10.3 and later).
+                        str = EndpointName((OpaqueMIDIEndpoint*)(connObject), true);
+                    } else {
+                        // Connected to an external device (10.2) (or something else, catch-all)
+                        str = NULL;
+                        MIDIObjectGetStringProperty(connObject, kMIDIPropertyName, &str);
+                    }
+                    if (str != NULL) {
+                        if (anyStrings)
+                            CFStringAppend(result, CFSTR(", "));
+                        else anyStrings = true;
+                        CFStringAppend(result, str);
+                        CFRelease(str);
+                    }
+                }
+            }
+        }
+        CFRelease(connections);
+    }
+    if (anyStrings)
+        return result;
+    
+    // Here, either the endpoint had no connections, or we failed to obtain names for any of them.
+    return EndpointName(endpoint, false);
+}
+    
 // This call creates the Graph and the Synth unit...
 OSStatus    CreateAUGraph (AUGraph &outGraph, AudioUnit &outSynth)
 {
+    // -----------------------------------
+    MIDIClientRef	theMidiClient;
+    MIDIClientCreate(CFSTR("Play Sequence"), NULL, NULL, &theMidiClient);		
+    ItemCount destCount = MIDIGetNumberOfDestinations();
+    printf("[AUNotePlayer] %i MIDI destinations\n", (int)destCount);
+    for (int n=0; n<(int)destCount; n++)
+    {
+        MIDIEndpointRef ref = MIDIGetDestination(n);
+        CFStringRef nameCStr = ConnectedEndpointName(ref);
+        char buffer[256];
+        CFStringGetCString(nameCStr, buffer, 256, kCFStringEncodingISOLatin1);
+        printf("[AUNotePlayer]   - '%s'\n", buffer);
+    }
+    // require_noerr (result = MusicSequenceSetMIDIEndpoint (sequence, MIDIGetDestination(0)), fail);
+    // -----------------------------------
+    
     OSStatus result;
     //create the nodes of the graph
     AUNode synthNode, limiterNode, outNode;
