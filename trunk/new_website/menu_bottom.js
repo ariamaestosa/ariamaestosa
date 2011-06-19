f5806c53409178b10137738e3e4593ad81cfc3d1
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2011-06-19 00:44:06 +0000

    More work on new website

--- trunk/new_website/menu_bottom.js
+++ trunk/new_website/menu_bottom.js
@@ -1,4 +1,4 @@
 
 
 document.write('</div><!--');
-document.write('--><div class="bottom"><br/><br/>Aria Maestosa is released under the GNU GPL 2 license<br/>We are freely hosted by <a href="http://sourceforge.net">sourceforge.net</a></div></center>');
+document.write('--><div class="bottom"><br/><br/>Aria Maestosa is released under the GNU GPL 2 license<br/>We are freely hosted by <a href="http://sourceforge.net">sourceforge.net</a></div></div>');
