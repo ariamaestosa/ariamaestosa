de15d38d961a128552ec6d822404f916538672d9
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2011-07-10 18:35:41 +0000

    Minor menu label clarification

--- trunk/Src/GUI/MainFrameMenuBar.cpp
+++ trunk/Src/GUI/MainFrameMenuBar.cpp
@@ -190,7 +190,7 @@ void MainFrame::initMenuBar()
 
 
     //I18N: menu item in the "edit" menu
-    m_edit_menu -> QUICK_ADD_MENU ( MENU_EDIT_PASTE_AT_CURSOR, wxString(_("Paste at cu&rsor"))+wxT("\tCtrl-Shift-V"), MainFrame::menuEvent_pasteAtMouse );
+    m_edit_menu -> QUICK_ADD_MENU ( MENU_EDIT_PASTE_AT_CURSOR, wxString(_("Paste at Mouse Cu&rsor"))+wxT("\tCtrl-Shift-V"), MainFrame::menuEvent_pasteAtMouse );
     m_edit_menu->AppendSeparator(); // ----- selection
     //I18N: menu item in the "edit" menu
     m_edit_menu -> QUICK_ADD_MENU ( MENU_EDIT_SELECT_ALL, wxString(_("Select &All"))+wxT("\tCtrl-A"), MainFrame::menuEvent_selectAll );
