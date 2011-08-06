95b6658a6c0ba00f48284409bcd1e51664a3e44a
auria <48419909+mg186@users.noreply.github.com>
2011-08-06 18:52:57 +0000

    After feedback from ln- on IRC, remove -s switch

--- international/update_pot.sh
+++ international/update_pot.sh
@@ -4,4 +4,4 @@ echo $CPP_FILE_LIST
 H_FILE_LIST=`find . -name '*.h' -print`
 echo $H_FILE_LIST
 
-xgettext -d AriaS -s --keyword=_ --add-comments="I18N:" -p ./international -o aria_maestosa.pot $CPP_FILE_LIST $H_FILE_LIST
+xgettext -d AriaS --keyword=_ --add-comments="I18N:" -p ./international -o aria_maestosa.pot $CPP_FILE_LIST $H_FILE_LIST
