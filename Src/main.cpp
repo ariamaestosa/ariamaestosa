59a5c48fc67c2667e1b8282da78c24868ddc9e59
auria <48419909+mg186@users.noreply.github.com>
2012-04-15 14:01:34 +0000

    Remove stupid Windows console code. It is sufficient to build without -mwindows

--- Src/main.cpp
+++ Src/main.cpp
@@ -44,77 +44,6 @@ END_EVENT_TABLE()
 // ------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------
 
-#ifdef __WXMSW__
-#include <windows.h>
-
-#include <stdio.h>
-#include <fcntl.h>
-#include <io.h>
-
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-
-// maximum mumber of lines the output console should have
-static const WORD MAX_CONSOLE_LINES = 500;
-
-#ifdef _MORE_DEBUG_CHECKS
-
-void RedirectIOToConsole()
-{
-    int hConHandle;
-    long lStdHandle;
-    
-    CONSOLE_SCREEN_BUFFER_INFO coninfo;
-    FILE *fp;
-    
-    // allocate a console for this app
-    AllocConsole();
-    
-    // set the screen buffer to be big enough to let us scroll text
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
-    coninfo.dwSize.Y = MAX_CONSOLE_LINES;
-    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
-    
-    // redirect unbuffered STDOUT to the console
-    lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    
-    fp = _fdopen( hConHandle, "w" );
-    *stdout = *fp;
-    setvbuf( stdout, NULL, _IONBF, 0 );
-    
-    // redirect unbuffered STDIN to the console
-    lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    
-    fp = _fdopen( hConHandle, "r" );
-    *stdin = *fp;
-    setvbuf( stdin, NULL, _IONBF, 0 );
-    
-    // redirect unbuffered STDERR to the console
-    
-    lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    fp = _fdopen( hConHandle, "w" );
-    
-    *stderr = *fp;
-    setvbuf( stderr, NULL, _IONBF, 0 );
-    
-    // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
-    // point to console as well
-    
-    ios::sync_with_stdio();
-}
-
-#endif
-#endif
-
-// ------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------
-
 void wxWidgetApp::activateRenderLoop(bool on)
 {
     if (on and not m_render_loop_on)
@@ -159,14 +88,6 @@ bool wxWidgetApp::OnInit()
 {   
     wxLogVerbose( wxT("wxWidgetsApp::OnInit (enter)") );
     m_render_loop_on = false;
-    
-    #ifdef __WXMSW__
-    #ifdef _MORE_DEBUG_CHECKS
-
-    RedirectIOToConsole();
-    
-    #endif
-    #endif
     
     frame = NULL;
     
