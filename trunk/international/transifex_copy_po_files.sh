f4f0de34fc43b1c66c7383533e629559a303bde8
auria <auria@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2012-04-17 19:26:07 +0000

    Updating translations should also update OSX lprojs

--- trunk/international/transifex_copy_po_files.sh
+++ trunk/international/transifex_copy_po_files.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ROOT=`pwd`
 cd transifex/translations/ariamaestosa.english-pot-file/
-for i in *.po; do echo ${i%%.*}/aria_maestosa.po && cp $i $ROOT/${i%%.*}/aria_maestosa.po && msgfmt -o $ROOT/${i%%.*}/aria_maestosa.mo $ROOT/${i%%.*}/aria_maestosa.po; done
+for i in *.po; do echo ${i%%.*}/aria_maestosa.po && cp $i $ROOT/${i%%.*}/aria_maestosa.po && msgfmt -o $ROOT/${i%%.*}/aria_maestosa.mo $ROOT/${i%%.*}/aria_maestosa.po && cp $ROOT/${i%%.*}/aria_maestosa.mo $ROOT/../OSX/mac-i18n/${i%%.*}.lproj/aria_maestosa.mo; done
