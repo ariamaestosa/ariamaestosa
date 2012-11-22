248f09da9ed2b088df83a27c83f64c9b58e44ce2
aarchambault <aarchambault@00197f5c-9b84-4722-8bc4-c547d54e2b44>
2012-11-22 17:11:50 +0000

    Fixed : warning on unused variable

--- trunk/libjdkmidi/src/jdksmidi_advancedsequencer.cpp
+++ trunk/libjdkmidi/src/jdksmidi_advancedsequencer.cpp
@@ -688,7 +688,7 @@ void AdvancedSequencer::ExtractMarkers ( std::vector< std::string > *list )
     int timesig_numerator = 4;
     int timesig_denominator = 4;
     MIDIClockTime last_beat_time = 0;
-    MIDIClockTime last_event_time = 0;
+    //MIDIClockTime last_event_time = 0;
     int clks_per_beat = tracks.GetClksPerBeat();
 
     for ( int i = 0; i < t->GetNumEvents(); ++i )
@@ -734,7 +734,7 @@ void AdvancedSequencer::ExtractMarkers ( std::vector< std::string > *list )
                 }
             }
 
-            last_event_time = m->GetTime();
+            //last_event_time = m->GetTime();
         }
     }
 
