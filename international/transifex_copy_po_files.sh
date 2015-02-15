36648ffa0ea4e89f71d459436c2e93ce96d0318f
auria <48419909+mg186@users.noreply.github.com>
2015-02-15 16:50:01 +0000

    Update translations

--- international/transifex_copy_po_files.sh
+++ international/transifex_copy_po_files.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ROOT=`pwd`
 cd transifex/translations/ariamaestosa.english-pot-file/
-for i in *; do echo ${i%%.*}/aria_maestosa.po && cp $i $ROOT/${i%%.*}/aria_maestosa.po && msgfmt -o $ROOT/${i%%.*}/aria_maestosa.mo $ROOT/${i%%.*}/aria_maestosa.po && cp $ROOT/${i%%.*}/aria_maestosa.mo $ROOT/../OSX/mac-i18n/${i%%.*}.lproj/aria_maestosa.mo; done
+for i in *; do echo ${i%%.*}/aria_maestosa.po && mkdir -p $ROOT/${i%%.*} && mkdir -p $ROOT/../OSX/mac-i18n/${i%%.*}.lproj && cp $i $ROOT/${i%%.*}/aria_maestosa.po && msgfmt -o $ROOT/${i%%.*}/aria_maestosa.mo $ROOT/${i%%.*}/aria_maestosa.po && cp $ROOT/${i%%.*}/aria_maestosa.mo $ROOT/../OSX/mac-i18n/${i%%.*}.lproj/aria_maestosa.mo; done
